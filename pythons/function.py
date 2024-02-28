8918cd2017bfb5a72139a16aea4878b19e598ac9
unknown <dinesh683adhikari@gmail.com>
2024-02-28 06:47:22 +0545

    how

--- pythons/function.py
+++ pythons/function.py
@@ -22,4 +22,4 @@ def multiply(a, b, c):
     return a * b * c
 
 
-print(multiply(2, 3, 4))
+hello = "kxa"
--- pythons/lists1.py
+++ pythons/lists1.py
@@ -1,8 +1,11 @@
-a = input("please enter any thing")
-b = input("please enter any thing")
-c = input("please enter any thing")
-d = []
-d.append(a)
-d.append(b)
-d.append(c)
-print(d)
+# a = input("please enter any thing")
+# b = input("please enter any thing")
+# c = input("please enter any thing")
+# d = []
+# d.append(a)
+# d.append(b)
+# d.append(c)
+# print(d)
+d = [1, 2, 3, 4, 5, 6, [1, 2, 3, 4]]
+a = d[6][1]
+print(a)
